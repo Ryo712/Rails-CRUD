9920e5b2dadaa82503f558d18ee70d9eee4ed543
大城亮 <hiltoc.katsu@gmail.com>
2025-10-13 16:06:34 +0900

    bodyの内容が2回表示されていたので重複箇所削除

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,13 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 3} #bodyの文字数3字以上。
+  has_one_attached :image
+end
+
+def image_content_type
+  if image.attached? && !image.content_type.in?(%w[image/jpeg image/png image/gif])
+    errors.add(:image, '：ファイル形式が、JPEG, PNG, GIF以外になってます。ファイル形式をご確認ください。')
+  end
+
+  
 end
